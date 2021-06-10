bc4fc4b789921a7a2dbfec7ecd234dce02f43cbf
Kaelan Cooter <dcgamers@gmail.com>
2021-06-10 11:51:53 -0400

    added trackpad support to camera

--- scenes/GameView/MapCamera.gd
+++ scenes/GameView/MapCamera.gd
@@ -8,31 +8,46 @@ var pan_speed = 600
 
 var _mouse_captured = false
 func _input(event):
-	# mousewheel zoom
+	# mouse wheel zoom
 	if event is InputEventMouseButton:
 		if event.is_action_pressed("view_zoom_in"):
 			zoom /= 1 + zoom_step
 		if event.is_action_pressed("view_zoom_out"):
 			zoom *= 1 + zoom_step
+
+	# trackpad pinch zoom
+	if event is InputEventMagnifyGesture:
+		if event.factor < 1:
+			zoom /= 1 + (zoom_step / 3)
+		else:
+			zoom *= 1 + (zoom_step / 3)
 	
+	# trackpad panning
+	if event is InputEventPanGesture:
+		position += event.delta * 25
+	
+	# mouse middle click panning
 	if event.is_action_pressed("view_pan_mouse"):
 		_mouse_captured = true
 	elif event.is_action_released("view_pan_mouse"):
 		_mouse_captured = false
-	
 	if _mouse_captured && event is InputEventMouseMotion:
-		position -= event.relative * zoom #opposite to relative motion, like we're grabbing the map
+		position -= event.relative * zoom
 
 func _process(delta):
+	var speed = 1
+	if Input.is_action_pressed("view_pan_fast"):
+		speed = 2
+	
 	var panning = Vector2()
 	if Input.is_action_pressed("view_pan_up"):
-		panning.y -= 1
+		panning.y -= speed
 	if Input.is_action_pressed("view_pan_down"):
-		panning.y += 1
+		panning.y += speed
 	if Input.is_action_pressed("view_pan_left"):
-		panning.x -= 1
+		panning.x -= speed
 	if Input.is_action_pressed("view_pan_right"):
-		panning.x += 1
+		panning.x += speed
 	
 	if panning.length_squared() > 0:
-		position += panning.normalized() * pan_speed * delta * zoom
+		position += panning * pan_speed * delta * zoom
