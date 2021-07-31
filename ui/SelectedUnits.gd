f9c2739d42c4aad32fc0b5514a53fa9218dccb1a
Kaelan Cooter <dcgamers@gmail.com>
2021-07-31 17:28:08 -0400

    main rect & selected units UI

--- ui/SelectedUnits.gd
+++ ui/SelectedUnits.gd
@@ -1,9 +1,13 @@
-extends ScrollContainer
+extends PanelContainer
 
 onready var UnitPanelContainer: Node = find_node("UnitPanelContainer")
 onready var UnitPanel = preload("res://ui/UnitPanel.tscn")
 
+
+onready var mim_size = rect_size  # get initial size when ready
+
 func _ready():
+	shrink_size()
 	MapManager.selected_units.subscribe(self, "_on_selected_units_update")
 
 
@@ -19,3 +23,6 @@ func _on_selected_units_update(selected_units):
 
 func _unit_panel_deselect(unit):
 	MapManager.deselect_unit(unit)
+
+func shrink_size():
+	rect_size = mim_size
